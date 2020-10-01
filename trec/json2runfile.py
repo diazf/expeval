a901444086537761dcd55b43ae1228b0a0cfd982
Fernando Diaz <diazf@acm.org>
2020-10-01 11:23:18 -0400

    final changes before results released for 2020

--- trec/json2runfile.py
+++ trec/json2runfile.py
@@ -3,14 +3,19 @@
 import argparse
 import json
 import csv
+import os
 
 def main():
     parser = argparse.ArgumentParser(description='convert fair ranking runfile json to trec runfile')
-    parser.add_argument('-I', dest='runfile')
+    parser.add_argument('-I', dest='runfile', required=True)
+    parser.add_argument('-r', dest='run_id')
 
     args = parser.parse_args()
-
     runfile = args.runfile
+    if not(args.run_id != None):
+        run_id = os.path.basename(runfile)
+    else:
+        run_id = args.run_id
     qid_cnts = {}
     with open(runfile,"r") as fp:
         for line in fp:
@@ -22,8 +27,15 @@ def main():
                 qid_cnts[qid] = qid_cnts[qid] + 1
             qid_idx = qid_cnts[qid]
             rank = 1
-            for did in data["ranking"]:                
-                print("%s\tQ%d\t%s\t%d\t%f"%(qid, qid_idx, did, rank, 1.0/rank))
+            for did in data["ranking"]:
+                row = []
+                row.append(qid)
+                row.append("Q%d"%qid_idx)
+                row.append(did)
+                row.append("%d"%rank)
+                row.append("%f"%(1.0/rank))
+                row.append(run_id)
+                print("\t".join(row))
                 rank = rank + 1
         
 if __name__ == '__main__':
